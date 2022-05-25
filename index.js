f40c62bc2e844ed1b81ccfd34eddb51da1a2be3b
urbanDev <urbanoakoro1991@gmail.com>
2022-05-25 14:27:59 +0200

    Este es el primer commit de este proyecto

---
+++ index.js
@@ -0,0 +1,22 @@
+function verificarRespuestas() {
+    var total = 5;
+    var puntos = 0;
+
+    var myForm = document.forms["quizForm"]; // referencia de las preguntas
+    var respuestas = ["a", "b","c", "a", "b"]; //aqui valido las respuestas de las preguntas correctas.
+
+    for (var i = 1; i <= total; i++) {
+        if(myForm["p" + i].value === null || myForm["p" + i].value === "") {  // Si dejamos alguna pregunta sin responder
+
+          document.getElementById("aviso").innerHTML = ('Por favor responde la pregunta '  + i);  // Nos muestra este alert del aviso que nos falta pregunta que responder.
+            return false;
+        } else{
+            if(myForm["p" + i].value === respuestas[i - 1]) { // ponemos i - 0 porque queremos conocer la posicion del array de cada pregunta
+                puntos++;
+            }
+        }
+    }
+
+     document.getElementById('resultado').innerHTML = `obtuviste ${puntos} de ${total}`;
+    return false
+}
